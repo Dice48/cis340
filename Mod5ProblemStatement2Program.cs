3fa544874d5484b3860e6b8b1fa808c91aac0dfe
Daisuke Aota <89590176+Dice48@users.noreply.github.com>
2021-09-27 22:11:39 -0400

    Add files via upload

---
+++ Mod5ProblemStatement2Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mod5ProblemStatement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+
+                int int1, int2;
+                Console.Write("\n\n");
+                Console.Write("Check whether two integers are greater then 18th or not:\n");
+                Console.Write("-------------------------------------------");
+                Console.Write("\n\n");
+                Console.Write("Enter the current time as a number between 1 and 24: ");
+               int1 = Convert.ToInt32(Console.ReadLine());
+
+                 Console.Write("Enter the number 18: ");
+                 int2 = Convert.ToInt32(Console.ReadLine());
+
+              if (int1 >= int2)
+                   Console.WriteLine("Good Evening, sir\n", int1, int2);
+              else
+                  Console.WriteLine("maam\n", int1, int2);
+        }   
+    }
+}
